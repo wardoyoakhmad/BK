06e55b065ecc8e0a10a460831a92e230d11c6d0c
aziz akhmad <azizakhmad.komsi2012@gmial.com>
2015-02-05 20:29:05 +0700

    upload view dan model

--- application/views/edit.php
+++ application/views/edit.php
@@ -3,158 +3,105 @@
       *********************************************************************************************************************************************************** -->
       <!--main content start-->
       <section id="main-content">
-          <section class="wrapper">
-            <h3><i class="fa fa-angle-right"></i> Edit Data</h3>
-            
+        <section class="wrapper">
+          <h3><i class="fa fa-angle-right"></i> Edit Data</h3>
+
           <?php
-            if(isset($breadcrumb)&& is_array($breadcrumb) && count($breadcrumb) > 0){
-          ?>
+          if(isset($breadcrumb)&& is_array($breadcrumb) && count($breadcrumb) > 0){
+            ?>
             <ul class="breadcrumb" id="crumbData">
-          <?php
-            foreach ($breadcrumb as $key=>$value) {
-            if($value!=''){
-          ?>
-            <li><a href="<?php echo $value; ?>"><?php echo $key; ?></a> <span class="divider"></span></li>
-          <?php }else{?>
-            <li class="active"><?php echo $key; ?></li>
-          <?php }
+              <?php
+              foreach ($breadcrumb as $key=>$value) {
+                if($value!=''){
+                  ?>
+                  <li><a href="<?php echo $value; ?>"><?php echo $key; ?></a> <span class="divider"></span></li>
+                  <?php }else{?>
+                  <li class="active"><?php echo $key; ?></li>
+                  <?php }
+                }
+                ?>     
+              </ul>
+              <?php
             }
-          ?>     
-            </ul>
-          <?php
-            }
-          ?>
+            ?>
             
             <div class="row mt">
-                  <div class="col-md-12">
-                      <div class="content-panel">
-                        <div class="row ms">
-                        <form class="form-inline" role="form">
-                        <div class="form-group">
+              <div class="col-md-12">
+                <div class="content-panel">
+                  <div class="row ms">
+                    <form class="form-inline" role="form">
+                      <div class="form-group">
                         <label class="control-label" for="select">Tahun</label>
-                               <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Enter email">
-                       
-                        </div>
-                          
-                             
-                              <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password">
-                              <select class="form-control" id="select">
+                        <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Enter email">
+
+                      </div>
+
+
+                      <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password">
+                      <select class="form-control" id="select">
                         <option>NIS</option>
                         <option>Nama</option>
                         <option>Kelas</option>
                         <option>Keterangan</option>
                         <option>No. HP</option>
-                        </select>
-                          
-                      </form>
-                      <br>
-                      <form class="form-inline" role="form">
-                        <div class="form-group">
+                      </select>
+                    </form>
+
+                    <br>
+                    <form class="form-inline" role="form" action="<?php echo base_url();?>siswa/input_kelas" method="post">
+                      <div class="form-group">
                         <label class="control-label" for="select">Kelas</label>
                         &nbsp
-                        <select class="form-control" id="select">
-                        <option>X</option>
-                        <option>XI</option>
-                        <option>XII</option>
-                       
+                        <select name="kelas" class="form-control" id="select">
+                        <?php foreach ($kelas as $data) {
+                         ?>
+                          <option value="<?php echo $data['id_kelas'];?>"><?php echo $data['nama_kelas']; ?></option>
+                          <?php } ?>
                         </select>
-                        </div>
-                      </form>
+                         &nbsp
+                        <select name="semester" class="form-control" id="select">
+                        <?php foreach ($semester as $data) {
+                         ?>
+                          <option value="<?php echo $data['id_tahun_ajaran'];?>"><?php echo $data['nama_semester']; ?></option>
+                          <?php } ?>
+                        </select>
                       </div>
-                          <table class="footable table-bordered table-striped">
-                            
-                              <thead>
-                              <tr>
-                                  <th><i class="fa fa-bullhorn"></i> Company</th>
-                                  <th class="hidden-phone"><i class="fa fa-question-circle"></i> Descrition</th>
-                                  <th><i class="fa fa-bookmark"></i> Profit</th>
-                                  <th><i class=" fa fa-edit"></i> Status</th>
-                                  <th></th>
-                              </tr>
-                              </thead>
-                              <tbody>
-                              <tr>
-                                  <td><a href="basic_table.html#">Company Ltd</a></td>
-                                  <td class="hidden-phone">Lorem Ipsum dolor</td>
-                                  <td>12000.00$ </td>
-                                  <td><span class="label label-info label-mini">Due</span></td>
-                                  <td>
-                                      <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
-                                      <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
-                                      <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
-                                  </td>
-                              </tr>
-                              <tr>
-                                  <td>
-                                      <a href="basic_table.html#">
-                                          Dashgum co
-                                      </a>
-                                  </td>
-                                  <td class="hidden-phone">Lorem Ipsum dolor</td>
-                                  <td>17900.00$ </td>
-                                  <td><span class="label label-warning label-mini">Due</span></td>
-                                  <td>
-                                      <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
-                                      <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
-                                      <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
-                                  </td>
-                              </tr>
-                              <tr>
-                                  <td>
-                                      <a href="basic_table.html#">
-                                          Another Co
-                                      </a>
-                                  </td>
-                                  <td class="hidden-phone">Lorem Ipsum dolor</td>
-                                  <td>14400.00$ </td>
-                                  <td><span class="label label-success label-mini">Paid</span></td>
-                                  <td>
-                                      <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
-                                      <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
-                                      <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
-                                  </td>
-                              </tr>
-                              <tr>
-                                  <td>
-                                      <a href="basic_table.html#">
-                                          Dashgum ext
-                                      </a>
-                                  </td>
-                                  <td class="hidden-phone">Lorem Ipsum dolor</td>
-                                  <td>22000.50$ </td>
-                                  <td><span class="label label-success label-mini">Paid</span></td>
-                                  <td>
-                                      <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
-                                      <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
-                                      <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
-                                  </td>
-                              </tr>
-                              <tr>
-                                  <td><a href="basic_table.html#">Total Ltd</a></td>
-                                  <td class="hidden-phone">Lorem Ipsum dolor</td>
-                                  <td>12120.00$ </td>
-                                  <td><span class="label label-warning label-mini">Due</span></td>
-                                  <td>
-                                      <button class="btn btn-success btn-xs"><i class="fa fa-check"></i></button>
-                                      <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button>
-                                      <button class="btn btn-danger btn-xs"><i class="fa fa-trash-o "></i></button>
-                                  </td>
-                              </tr>
-                              </tbody>
-                              <tfoot>
-    <tr>
-      <td colspan="5">
-        <div class="pagination pagination-centered hide-if-no-paging"></div>
-      </td>
-    </tr>
-  </tfoot>
-                          </table>
-                      </div><!-- /content-panel -->
-                  </div><!-- /col-md-12 -->
-              </div><!-- /row -->
+                    </div>
+                    <button type="submit">daftar</button>
+                    <table class="footable table-bordered table-striped">
+
+                      <thead>
+                        <tr>
+                          <th><i class="fa fa-bullhorn"></i> NIS</th>
+                          <th class="hidden-phone"><i class="fa fa-question-circle"></i> Nama</th>
+                          <th><i class="fa fa-bookmark"></i> Cek List</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                      <?php foreach ($siswa as $data) {$nis = $data['nis'];?>
+                        <tr>
+                          <td><?php echo $data['nis']; ?></td>
+                          <td><?php echo $data['nama'];?></td>
+                          <td><input type="checkbox" name="nis[]" value="<?php echo $data['nis'];
+                           ?>"></td>
+                        </tr>
+                        <?php } ?>
+                      </tbody>
+                      <tfoot>
+                        <tr>
+                          <td colspan="5">
+                            <div class="pagination pagination-centered hide-if-no-paging"></div>
+                          </td>
+                        </tr>
+                      </tfoot>
+                    </table>
+                  </form>
+                </div><!-- /content-panel -->
+              </div><!-- /col-md-12 -->
+            </div><!-- /row -->
             
             
-    </section><! --/wrapper -->
-      </section><!-- /MAIN CONTENT -->
+          </section><! --/wrapper -->
+        </section><!-- /MAIN CONTENT -->
 
       <!--main content end-->
--- application/views/login.php
+++ application/views/login.php
@@ -1,83 +1,79 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
+<html>
+<head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=1,initial-scale=1,user-scalable=1" />
     <title>Login</title>
     <!-- Custom CSS -->
     <link rel="shortcut icon" href="<?= base_url().'asset/images/logo.png'?>"/>
+    <link type="text/css" rel="stylesheet" href="<?= base_url().'asset/css/login.css';?>"/>
+    <!-- Google Font -->
 
+    <!-- Bootstrap Core CSS -->
     <link type="text/css" rel="stylesheet" href="<?= base_url().'asset/css/bootstrap.min.css';?>"/>
     <link type="text/css" rel="stylesheet" href="<?= base_url().'asset/bootflat/css/bootflat.min.css';?>"/>
     <link type="text/css" rel="stylesheet" href="<?= base_url().'asset/fontawesome/css/font-awesome.min.css';?>"/>
-    <link href="<?= base_url().'asset/css/pagestyle.css';?>" rel="stylesheet">
-    <link href="<?= base_url().'asset/css/pagestyle-responsive.css';?>" rel="stylesheet">
+    
+</head>
+<body>
 
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
+    <section class="container">
+        <section class="login-form">
+            <form method="post" action="<?php echo base_url(); ?>login/masuk" role="login" id="loginForm">
 
-  <body>
-
-      <div id="login-page">
-        <div class="container">
-        
-              <form class="form-login" method="post" action="<?php echo base_url(); ?>login/masuk" role="login">
-                <h2 class="form-login-heading">Login</h2>
-                <br>
-                <div class="login-wrap">
-                    <input type="text" name="email" class="form-control" placeholder="Username" required class="form-control input-lg" autofocus>
-                    <br>
-                    <input type="password" name="pass" class="form-control" placeholder="Password" required class="form-control input-lg">
-                    <br>
-                    <button class="btn btn-success btn-block" type="submit"><i class="fa fa-lock"></i> Login</button>
-                    <br>
-                   
-                    <span class="pull-right">
-                        <a data-toggle="modal" href="login.html#myModal"> Lupa Password?</a>
-                    </span>
-                    <br>
+                <div class="container" id="loginBody">
+                    <h3 id="loginTitle"><b>-PROTOTYPE-</b></h3>
+                    <div class="left-inner-addon">
+                        <i class="fa fa-user"></i>      
+                        <input type="text" name="email" placeholder="Nama" required class="form-control input-lg" />
                     </div>
-                    <p class="form-login-footer">copyright <i class="fa fa-copyright"></i> 2015
-                        blackomsi</p>
+                    <div class="left-inner-addon">
+                        <i class="fa fa-lock"></i>
+                        <input type="password" name="pass" placeholder="Password" required class="form-control input-lg" />
                     </div>
-        
-                  <!-- Modal -->
-                  <div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="myModal" class="modal fade">
-                      <div class="modal-dialog">
-                          <div class="modal-content">
-                              <div class="modal-header">
-                                  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                                  <h4 class="modal-title">Lupa Password ?</h4>
-                              </div>
-                              <br>
-                              <div class="modal-body">
-                                  <p>Masukkan alamat email untuk me-reset password</p>
-                                  <input type="text" name="email" placeholder="Email" autocomplete="off" class="form-control placeholder-no-fix">
-        
-                              </div>
-                              <div class="modal-footer">
-                                  <button data-dismiss="modal" class="btn btn-default" type="button">Cancel</button>
-                                  <button class="btn btn-success" type="button">Submit</button>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-                  <!-- modal -->
-        
-              </form>       
-        
-        </div>
-      </div>
+                    <button type="submit" class="btn btn-lg btn-block btn-info"><i class="fa fa-sign-in"></i> Masuk</button>
+                    <div>
+                        <p><a href="#">Lupa Password ?</a></p>
+                    </div>
+                    <div>
+                        <p id="catatanKaki">copyright <i class="fa fa-copyright"></i> 2015. blackomsi</p>
+                    </div>
+                </div>
 
-    <script src="<?= base_url().'asset/js/jquery.js';?>"></script>
+            </form>
+        </section>
+    </section>
+
+    <script src="<?= base_url().'asset/js/jquery-1.11.2.min.js';?>"></script>
+    <script src="<?= base_url().'asset/js/jquery.validate.min.js';?>"></script>
+    <script src="<?= base_url().'asset/js/jqueryvalidate.js';?>"></script>
     <script src="<?= base_url().'asset/js/bootstrap.min.js';?>"></script>
 
-    <script type="text/javascript" src="<?= base_url().'asset/js/jquery.backstretch.min.js'?>"></script>
+    <script type="text/javascript">
+     $("#loginForm").validate({
+        rules: {
+            email: {
+                required: true
+            },
+            pass: {
+                required: true
+            }
+        },
 
-  </body>
+        messages: {
+            pass: "Pesan error.",
+            email: "Pesan error."
+        },
+
+        tooltip_options: {
+            email: {
+                placement:'bottom',html:true},
+                pass: {
+                    placement:'bottom',html:true}
+                },
+            });
+ </script>
+ 
+</body>
 </html>
