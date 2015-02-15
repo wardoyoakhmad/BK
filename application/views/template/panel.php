8978e0a7fc9f1d7c79cfbb69d3dd9a0c641b02e8
proyeksi <rizalbrotherrepo@gmail.com>
2015-02-16 00:11:49 +0700

    Update panel.php

--- application/views/template/panel.php
+++ application/views/template/panel.php
@@ -23,13 +23,13 @@
                           <span>Data &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-chevron-right pull-right" id="panah"></i></span>
                       </a>
                       <ul class="sub">
-<<<<<<< HEAD
+
                           <li><a  href="<?php echo base_url();?>page/import">Import Data Siswa Baru</a></li>
-=======
+
                           <li><a  href="<?php echo base_url();?>page/absen">Absensi</a></li>
                           <li><a  href="<?php echo base_url();?>page/import">Import Data Siswa Baru</a></li>
                           <li><a  href="<?php echo base_url();?>page/semester">Tambah Semester</a></li>
->>>>>>> 4eb7f4c286601333acca784b360ffd11d3e58801
+
                           <li><a  href="<?php echo base_url();?>page/data">Data Siswa</a></li>
                           <li><a  href="<?php echo base_url();?>page/rekap">Rekap Data</a></li>
                           <li><a  href="<?php echo base_url();?>page/edit">Input Data Siswa</a></li>
@@ -41,14 +41,13 @@
                           <i class="fa fa-envelope"></i>
                           <span>Pesan</span>
                       </a>
-<<<<<<< HEAD
+
                       <ul class="sub">
                           <li><a  href="<?php echo base_url();?>pesan">Kirim Pesan</a></li>
                           <li><a  href="<?php echo base_url();?>page/">Kotak Masuk</a></li>
                           <li><a  href="<?php echo base_url();?>page/">Kotak Keluar</a></li>
                       </ul>
-=======
->>>>>>> 4eb7f4c286601333acca784b360ffd11d3e58801
+
                   </li>
                   
               </ul>
